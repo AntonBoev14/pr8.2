9abc545a517592c072d3806d66d59b4e946bd9ea
AntonBoev14 <antonboev421@gmail.com>
2024-11-07 10:08:18 +0300

    Add files via upload

---
+++ pr8.2/pr8.2.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+
+int main() {
+    setlocale(0, "ru");
+    int sum = 0; 
+    int current = 1; 
+    std::cout << "Последовательность: ";
+
+    while (current <= 256) {
+        std::cout << current << " "; 
+        sum += current; 
+        current *= 2; 
+    }
+
+    std::cout << "\nСумма элементов последовательности: " << sum << std::endl;
+
+    return 0;
+}
